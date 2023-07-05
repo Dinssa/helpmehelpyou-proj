87bc85553774b3075b5b5714840f982efdedaae2
dinssa <manny@dinssa.com>
2023-07-05 10:38:05 +0100

    Hash passwords and JWT responses

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,9 +1,20 @@
-module.exports = {
-    create
-};
 
-function create(req, res) {
-    // Baby step...
+const User = require('../../models/user');
+const jwt = require('jsonwebtoken');
+
+async function create(req, res) {
+    try{
+        // Attempt to create the user from request body
+        const user = await User.create(req.body);
+
+        // Generate a jwt that contains the user information and is signed with the secret
+        const token = createJWT(user);
+
+        return res.json(token);
+    } catch (err) {
+        return res.status(401).json(err);
+    }
+
     res.json({
         user: {
         name: req.body.name,
@@ -11,3 +22,16 @@ function create(req, res) {
         }
     });
 }
+
+
+function createJWT(user) {
+    return jwt.sign(
+        { user }, // data payload
+        process.env.SECRET,
+        { expiresIn: '24h' }
+    );
+}
+
+module.exports = {
+    create
+};
--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 12;
 
 // User model (basic implementation, only fields for authentication)
 
@@ -23,14 +25,30 @@ const userSchema = new mongoose.Schema({
         virtuals: true, // Include virtual properties when converting document to JSON
         transform: (doc, ret) => {
             delete ret.password; // Delete the password property when converting document to JSON
+            delete ret._id; // Delete the _id property when converting document to JSON
+            delete ret.__v; // Delete the __v property when converting document to JSON
             return ret;
         }
     }
 });
 
+// Just before saving out user, after validation and right before saving to DB, we want to hash the password
+// We'll use bcrypt to hash the password, but mongoose provides us with a pre hook so we can execute a callback function just before the user is created
+
+userSchema.pre('save', function (next) {
+    // 'this' will be set to the current document about to be saved
+    // We need to check if the password has been modified, because we don't want to hash it again (hash a hash) if it hasn't been modified
+    if (this.isModified('password')) {
+        // Hash the password
+        const hash = bcrypt.hashSync(this.password, SALT_ROUNDS);
+        // Reassign the password to the hashed version
+        this.password = hash;
+    }
+    // Call next() so that the next middleware in the chain can run
+    next();
+});
 
 
-
-// Always make sure to export the model last
+// ! Always make sure to export the model last
 // We assign the model to a variable so we can use it in other files, and use model methods to CRUD our data
 module.exports = mongoose.model('User', userSchema); // Export the model with the schema attached
