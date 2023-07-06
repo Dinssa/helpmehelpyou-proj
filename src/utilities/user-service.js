e2b6e3428916760ec362f63c68813c160e0eca66
dinssa <manny@dinssa.com>
2023-07-06 09:08:56 +0100

    Add token methods

--- src/utilities/user-service.js
+++ src/utilities/user-service.js
@@ -11,4 +11,26 @@ export async function signUp(userData) {
     localStorage.setItem('token', token);
 
     return token;
+}
+
+export function getToken() {
+  const token = localStorage.getItem('token');
+  if (!token) return null;
+  
+  // Check if expired, remove if it is
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  // A JWT's exp is expressed in seconds, not milliseconds, so convert
+  // to milliseconds here
+  if (payload.exp < Date.now() / 1000) {
+    // Remove the token from localStorage
+    localStorage.removeItem('token');
+    return null;
   }
+
+  return token;
+}
+
+export function getUserFromToken() {
+    const token = getToken();
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+}
