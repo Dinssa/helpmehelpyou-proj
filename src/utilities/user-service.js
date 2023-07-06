34643bc15319950028fba27110b4a1f9c2a6cb5b
dinssa <manny@dinssa.com>
2023-07-06 11:01:23 +0100

    Remove redundant code

--- src/utilities/user-service.js
+++ src/utilities/user-service.js
@@ -51,9 +51,6 @@ export function logOut() {
 }
 
 export function checkToken() {
-  // Just so that you don't forget how to use .then
   return usersAPI.checkToken()
-    // checkToken returns a string, but let's 
-    // make it a Date object for more flexibility
     .then(dateStr => new Date(dateStr));
 }
