c9dbceed8720ba4c0bddefa30223f0b9c8f4eefd
dinssa <manny@dinssa.com>
2023-07-12 16:55:58 +0100

    Create navbar from wireframe

--- src/components/global/Nav/Nav.jsx
+++ src/components/global/Nav/Nav.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import './Nav.css'
 import * as userService from '../../../utilities/users-service';
+import { Dropdown } from 'react-bootstrap';
 
 export default function Nav({user, setUser}){
 
@@ -9,25 +10,84 @@ export default function Nav({user, setUser}){
         setUser(null)
     }
 
+    console.log(user)
 
     return (
-        <nav>
-            <ul className="NavList">
-                <li className="NavItem">
-                    <Link to="/orders">Order History</Link>
-                </li>
-                <li className="NavItem">
-                    <Link to="/orders/new">New Order</Link>
-                </li>
-            </ul>
-            <ul className="AccountList">
-                <li className="NavItem">
-                    <span>Welcome, {user.name}</span>
-                </li>
-                <li className="NavItem">
-                    <Link to="" onClick={handleLogOut}>Log Out</Link>
-                </li>
-            </ul>
+        <nav className="bg-primary text-white">
+            <div>
+                <h3 className="ms-3 p-1">helpmehelpyou</h3>
+            </div>
+            
+            { user ? 
+                <>
+                <ul className="NavList">
+                    <li className="NavItem">
+                        <Link to=""><i className="fa-solid fa-bell"></i></Link>
+                    </li>
+                    <li className="NavItem">
+                        <Link to=""><i className="fa-solid fa-envelope"></i></Link>
+                    </li>
+
+                    { user.roles.includes('Freelancer') ?
+                        <>
+                        <li className="NavItem">
+                            <Link to="/projects">Projects</Link>
+                            </li>
+                            <li className="NavItem">
+                                <Link to="/templates">Templates</Link>
+                            </li>
+                        </>
+                        :
+                        <></>
+                    }
+
+                    { user.roles.includes('Client') ?
+                        <>
+                        <li className="NavItem">
+                            <Link to="/forms">Forms</Link>
+                        </li>
+                        </>
+                        :
+                        <></>
+                    }
+                </ul>
+                <div>
+                    { user.avatar ?
+                        <div className="avatar">
+                            <img src={user.avatar} alt="User avatar" className=""/>
+                        </div> 
+                    :   
+                        <div className="default-icon">
+                            <i className="fa-solid fa-user"></i>
+                        </div>
+                    }
+                </div>
+                <Dropdown id="nav-dropdown">
+                    <Dropdown.Toggle variant="" id="dropdown-btn">
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#">Profile</Dropdown.Item>
+                        <Dropdown.Item href="#">Settings</Dropdown.Item>
+                        <Dropdown.Item href="" onClick={handleLogOut}>Sign Out</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                </>
+                
+            :
+                <ul className="NavList">
+                    <li className="NavItem">
+                        <Link to="/">Clients</Link>
+                    </li>
+                    <li className="NavItem">
+                        <Link to="/">Freelancers</Link>
+                    </li>
+                    <li className="NavItem signin">
+                        <Link to="/auth">Sign In</Link>
+                    </li>
+                </ul>
+            }
+            
         </nav>
     )
 }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -8,25 +8,28 @@ import { getUserFromToken } from '../../utilities/users-service';
 
 // Custom components
 import AuthPage from '../Auth/AuthPage';
-import NewOrderPage from '../NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage';
 import Nav from '../../components/global/Nav/Nav';
+import HomePage from '../Home/HomePage';
+import ProjectPage from '../Project/ProjectPage';
+import TemplatePage from '../Template/TemplatePage';
 
 function App() {
   const [user, setUser] = useState(getUserFromToken())
   return (
     <main className="App">
+      <Nav user={user} setUser={setUser} />
       {user ? 
       <>
-        <Nav user={user} setUser={setUser} />
         <Routes>
-          <Route path="/orders/new" element={<NewOrderPage />} />
-          <Route path="/orders" element={<OrderHistoryPage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+          <Route path="/templates" element={<TemplatePage />} />
         </Routes>
       </>
-        
         :
-        <AuthPage setUser={setUser} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage setUser={setUser} />} />
+        </Routes>
       }
     </main>
   );
