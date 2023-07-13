553ca36b93a3aa26f1baf7a7448997ca65035581
dinssa <manny@dinssa.com>
2023-07-13 10:53:03 +0100

    Add search bar

---
+++ src/components/global/SearchBar/SearchBar.jsx
@@ -0,0 +1,16 @@
+import './SearchBar.css'
+import { Form, FormControl, Button } from 'react-bootstrap';
+
+export default function SearchBar({searchQuery, setSearchQuery}){
+    return (
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      )
+}
--- src/pages/Template/TemplatesPage.jsx
+++ src/pages/Template/TemplatesPage.jsx
@@ -1,5 +1,21 @@
 import './TemplatesPage.css'
+import { useState, useEffect } from 'react';
+
+// Components
+import TemplateList from '../../components/Template/TemplateList';
+import SearchBar from '../../components/global/SearchBar/SearchBar';
 
 export default function TemplatesPage(){
-    return <h1>Templates Page</h1>
+
+    const [searchQuery, setSearchQuery] = useState('')
+
+
+
+    return (
+        <main className="TemplatesPage">
+            <h1>TemplatesPage</h1>
+            <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+            <TemplateList searchQuery={searchQuery} />
+        </main>
+    )
 }
