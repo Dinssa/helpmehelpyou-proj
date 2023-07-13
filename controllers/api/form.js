e70c7518d343080efc98ee828369456e5af12f8b
dinssa <manny@dinssa.com>
2023-07-13 11:28:47 +0100

    Build out forms api

--- controllers/api/form.js
+++ controllers/api/form.js
@@ -2,14 +2,102 @@ const Form = require('../../models/form');
 
 module.exports = {
     create,
+    index,
+    show,
+    update,
+    userIndex,
+    userIndexNonArchived,
+    userIndexArchived,
+    delete: deleteOne,
 };
 
+// Create a form
 async function create(req, res) {
     try{
         const form = await Form.create(req.body);
 
         return res.json(form);
     } catch (err) {
-        return res.status(401).json(err);
+        return res.status(err.code || 401).json(err);
+    }
+}
+
+// Get all forms
+async function index(req, res) {
+    try{
+        const forms = await Form.find({});
+
+        return res.json(forms);
+    } catch (err) {
+        return res.status(err.code || 401).json(err);
+    }
+}
+
+// Get all forms that the user has access to
+async function userIndex(req, res) {
+    try{
+        const forms = await Form.find({sharedWith: req.user._id});
+
+        return res.json(forms);
+    } catch (err) {
+        return res.status(err.code || 401).json(err);
+    }        
+}
+
+// Get all forms that the user has access to, not archived
+async function userIndexNonArchived(req, res) {
+    try{
+        const forms = await Form.find({sharedWith: req.user._id, archived: false});
+
+        return res.json(forms);
+    } catch (err) {
+        return res.status(err.code || 401).json(err);
+    }
+}
+
+// Get all forms that the user has access to, archived
+async function userIndexArchived(req, res) {
+    try{
+        const forms = await Form.find({sharedWith: req.user._id, archived: true});
+
+        return res.json(forms);
+    } catch (err) {
+        return res.status(err.code || 401).json(err);
+    }
+}
+
+// Show a form
+async function show(req, res) {
+    try{
+        const form = await Form.findById(req.params.id);
+
+        return res.json(form);
+    } catch (err) {
+        return res.status(err.code || 401).json(err);
+    }
+}
+
+// Update a form
+async function update(req, res) {
+    try{
+        const form = await Form.findById(req.params.id);
+        if (!form) throw new Error('Form not found');
+        Object.assign(form, req.body);
+        await form.save();
+        return res.json(form);        
+    } catch (err) {
+        return res.status(err.code || 401).json(err);
+    }
+}
+
+// Delete a form
+async function deleteOne(req, res) {
+    try{
+        const form = await Form.findById(req.params.id);
+        if (!form) throw new Error('Form not found');
+        await form.remove();
+        return res.json(form);
+    } catch (err) {
+        return res.status(err.code || 401).json(err);
     }
 }
--- routes/api/forms.js
+++ routes/api/forms.js
@@ -2,6 +2,30 @@ const express = require('express');
 const router = express.Router();
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
+const formController = require('../../controllers/api/form');
 
+// Route for getting all forms
+router.get('/forms', ensureLoggedIn, formController.index);
+
+// Route for getting all forms that belong to the user
+router.get('/forms/user', ensureLoggedIn, formController.userIndex);
+
+// Route for getting all non-archived forms that belong to the user
+router.get('/forms/user/nonarchived', ensureLoggedIn, formController.userIndexNonArchived);
+
+// Route for getting all archived forms that belong to the user
+router.get('/forms/user/archived', ensureLoggedIn, formController.userIndexArchived);
+
+// Route for getting a form
+router.get('/forms/:id', ensureLoggedIn, formController.show);
+
+// Route for creating a form
+router.post('/forms', ensureLoggedIn, formController.create);
+
+// Route for updating a form
+router.put('/forms/:id', ensureLoggedIn, formController.update);
+
+// Route for deleting a form
+router.delete('/forms/:id', ensureLoggedIn, formController.delete);
 
 module.exports = router;
