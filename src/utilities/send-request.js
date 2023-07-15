7dc56e2b412f6a7f99e6395190b388d13c934220
dinssa <manny@dinssa.com>
2023-07-15 17:45:30 +0100

    Modify utility to also allow parameters

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,7 +1,7 @@
 import { getToken } from './users-service';
 
 
-export default async function sendRequest(url, method = 'GET', payload = null) {
+export default async function sendRequest(url, method = 'GET', payload = null, params = {}) {
     // Fetch accepts an options object as the 2nd argument
     // used to include a data payload, set headers, etc. 
     const options = { method };
@@ -17,7 +17,9 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
         // Prefacing with 'Bearer' is recommended in the HTTP specification
         options.headers.Authorization = `Bearer ${token}`;
     }
-    const res = await fetch(url, options);
+    // Add any query string parameters to the url
+    const urlParams = new URLSearchParams(params);
+    const res = await fetch(`${url}?${urlParams}`, options);
     // res.ok will be false if the status code set to 4xx in the controller action
     if (res.ok) return res.json();
     throw new Error('Bad Request');
